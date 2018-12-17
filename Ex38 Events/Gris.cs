a3e3a1cc03081de11815ec2e3c0447bdcdefe5a9
Peter Starup <pete59f1@edu.eal.dk>
2018-12-17 11:25:25 +0100

    Gris

---
+++ Ex38 Events/Gris.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex38_Events
+{
+    public class Gris
+    {
+        private double saldo = 0.0;
+        private int bill = 0;
+
+        public void InsertAmount(Metronome m, double amount)
+        {
+            do
+            {
+                m.Tick += new Metronome.dele(Saldo);
+                m.Tick += new Metronome.dele(OB);
+                saldo += amount;
+            } while (amount != 0);
+        }
+
+        public void Saldo(Metronome m, EventArgs e)
+        {
+            Console.WriteLine("Grisens saldo er nu: " + saldo);
+            bill = Convert.ToInt32(saldo / 595);
+        }
+        public void OB(Metronome m, EventArgs e)
+        {
+            switch (bill)
+            {
+                case 1:
+                    Console.WriteLine("Tillykke – du har nu penge nok til " + bill + " sæsonkort til OBs kampe");
+                    break;
+                case 2:
+                    Console.WriteLine("Tillykke – du har nu penge nok til " + bill + " sæsonkort til OBs kampe");
+                    break;
+                case 3:
+                    Console.WriteLine("Tillykke – du har nu penge nok til " + bill + " sæsonkort til OBs kampe");
+                    break;
+            }
+        }
+    }
+}
--- Ex38 Events/Program.cs
+++ Ex38 Events/Program.cs
@@ -10,22 +10,26 @@ namespace Ex38_Events
     {
         static void Main(string[] args)
         {
-            Metronome m = new Metronome();
-            Metronome m2 = new Metronome();
-            TickListener l = new TickListener();
-            //l.Sub(m);
-            //m.Loop();
-            Console.WriteLine("Write a name:");
-            m.Name = Console.ReadLine();
-            Console.WriteLine("\n");
-            Console.WriteLine("A second name:");
-            m2.Name = Console.ReadLine();
-            Console.WriteLine("\n");
+            //Metronome m = new Metronome();
+            //Metronome m2 = new Metronome();
+            //TickListener l = new TickListener();
+            ////l.Sub(m);
+            ////m.Loop();
+            //Console.WriteLine("Write a name:");
+            //m.Name = Console.ReadLine();
+            //Console.WriteLine("\n");
+            //Console.WriteLine("A second name:");
+            //m2.Name = Console.ReadLine();
+            //Console.WriteLine("\n");
 
-            l.LifeSub(m);
-            l.LifeSub(m2);
-            m.LifeLoop();
-            m2.LifeLoop();
+            //l.LifeSub(m);
+            //l.LifeSub(m2);
+            //m.LifeLoop();
+            //m2.LifeLoop();
+
+            Metronome m = new Metronome();
+            Gris g = new Gris();
+            g.InsertAmount(m, double.Parse(Console.ReadLine()));
         }
     }
 }
